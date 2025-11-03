abf6dc8d4b97cb629c0a80ee15ed0b5b26aa281d
Maamar NourAllah <nourallah.maamar@esprit.tn>
2025-11-03 15:30:54 +0100

    Save current work before merge

---
+++ dist/config/cloudinary.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cloudinaryConfig = {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME || '',
+    apiKey: process.env.CLOUDINARY_API_KEY || '',
+    apiSecret: process.env.CLOUDINARY_API_SECRET || '',
+};
+exports.default = cloudinaryConfig;
+//# sourceMappingURL=cloudinary.config.js.map
